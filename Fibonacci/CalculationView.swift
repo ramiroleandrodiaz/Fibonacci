7ba81162aa6a2b0713c68f4ac8a4f50c5572d8ce
Ramiro Leandro Diaz <ramiro.leandro.diaz@gmail.com>
2025-07-10 18:16:13 -0300

    Added legibility
    SeparatedListCalculationView into a different function

--- Fibonacci/CalculationView.swift
+++ Fibonacci/CalculationView.swift
@@ -16,50 +16,34 @@ struct CalculationView: View {
     var body: some View {
       NavigationStack {
         VStack {
-          VStack {
-            NumericTextfield(input: $viewModel.inputNumber, textfieldPrompt: "Enter a number between 0 and 90.")
-              .focused($isInputFocused)
-              .textFieldStyle(.roundedBorder)
-              .padding()
-              .overlay {
-                RoundedRectangle(cornerRadius: 8)
-                  .stroke(!viewModel.isValidInput(viewModel.inputNumber) ? Color.red : Color.clear, lineWidth: 2)
-                  .padding()
-              }
-              .toolbar {
-                ToolbarItem(placement: .keyboard) {
-                  Button("Done") {
-                    isInputFocused = false
-                    viewModel.calculateFibonacci()
-                  }
+          NumericTextfield(input: $viewModel.inputNumber, textfieldPrompt: "Enter a number between 0 and 90.")
+            .focused($isInputFocused)
+            .textFieldStyle(.roundedBorder)
+            .padding()
+            .overlay {
+              RoundedRectangle(cornerRadius: 8)
+                .stroke(!viewModel.isValidInput(viewModel.inputNumber) ? Color.red : Color.clear, lineWidth: 2)
+                .padding()
+            }
+            .toolbar {
+              ToolbarItem(placement: .keyboard) {
+                Button("Done") {
+                  isInputFocused = false
+                  viewModel.calculateFibonacci()
                 }
               }
-            
-            // Display Error Case
-            if !viewModel.isValidInput(viewModel.inputNumber) && !viewModel.inputNumber.isEmpty {
-              Text("Please enter a valid number between 0 and 90")
-                .errorLabelStyle()
-                .padding(.horizontal)
             }
+          
+          // Display Error Case
+          if !viewModel.isValidInput(viewModel.inputNumber) && !viewModel.inputNumber.isEmpty {
+            Text("Please enter a valid number between 0 and 90")
+              .errorLabelStyle()
+              .padding(.horizontal)
           }
           
           if let calculation = viewModel.currentCalculation {
             // Display Fibonacci sequence list
-            List {
-              Section(header: TwoColumnHeaderView(title: "Fibonacci Sequence",
-                                                  leftColumnTitle: "N",
-                                                  rightColumnTitle: "F(N)")) {
-                ForEach(calculation.sequence, id: \.index) { item in
-                  SingleSecuenceView(singleFibonacciElemet: item)
-                }
-              }
-            }
-            .listStyle(.plain)
-            
-            // Calculation time
-            Text(String(format: "Calculation time: %.4f ms", calculation.calculationTime))
-                .padding()
-                .background(Color(.systemBackground))
+            listCalculationView(calculation: calculation)
           } else {
             // Accomodate everything to the Top
             Spacer()
@@ -76,6 +60,28 @@ struct CalculationView: View {
           }
       }
     }
+  
+  
+  // Separating this function for more legibility
+  private func listCalculationView(calculation: FibonacciCalculation) -> some View {
+    VStack {
+      List {
+        Section(header: TwoColumnHeaderView(title: "Fibonacci Sequence",
+                                            leftColumnTitle: "N",
+                                            rightColumnTitle: "F(N)")) {
+          ForEach(calculation.sequence, id: \.index) { item in
+            SingleSecuenceView(singleFibonacciElemet: item)
+          }
+        }
+      }
+      .listStyle(.plain)
+      
+      // Calculation time
+      Text(String(format: "Calculation time: %.4f ms", calculation.calculationTime))
+          .padding()
+          .background(Color(.systemBackground))
+    }
+  }
 }
 
 #Preview {
